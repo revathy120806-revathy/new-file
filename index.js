49403b62f9af95152f8aabfefce4f8b9649cb093
revathy120806-revathy <revathy120806@gmail.com>
2025-12-03 02:54:10 +0530

    try

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import AOS from 'aos';
 const app=express()
 app.set("view engine","ejs")
 const pathName=path.resolve('assets')
-
+const imgPath=path.resolve('public')
 const folderName='img'
 app.use(express.static(path.join(pathName,folderName)))
 
@@ -17,9 +17,11 @@ app.get("/",(req,resp)=>{
  const absPath=path.resolve('index.html')
 resp.sendFile(absPath)
 console.log(absPath)
-const folderName='img'
-app.use('/img',express.static(path.join(pathName,'img')))
-console.log((path.join(pathName,folderName)));
+
+app.use(express.static(imgPath))
+console.log(imgPath)
+
+
 
 })
 
