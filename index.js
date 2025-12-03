6101f576ab3ae836465c2473c6efc0dd9925f614
revathy120806-revathy <revathy120806@gmail.com>
2025-12-04 05:04:48 +0530

    try 5

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ resp.sendFile(absPath)
 
 })
 
-app.post("/contact.php",async(req,resp)=>{
+app.post("/Datasheet.html",async(req,resp)=>{
 const name=req.body.name
 const email=req.body.email
 const subject=req.body.subject
