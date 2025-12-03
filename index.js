137859c0e3fc66717f59ed5fcae96c6e743fa157
revathy120806-revathy <revathy120806@gmail.com>
2025-12-03 23:34:26 +0530

    try 1

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ resp.sendFile(absPath)
 
 })
 
-app.post("/Datasheet",async(req,resp)=>{
+app.get("/Datasheet",async(req,resp)=>{
 const name=req.body.name
 const email=req.body.email
 const subject=req.body.subject
